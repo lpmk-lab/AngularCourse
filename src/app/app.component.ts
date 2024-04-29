bf43c9539642c6d825446c9d345eea35991d48a7
lapyaeminkhant.sbs <lapyaeminkhant.sbs@gmail.com>
2024-04-29 20:24:52 +0630

    merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularCourse';
+  title = ' APP';
+  message:string="Hello"
 }
